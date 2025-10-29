089e8191ae321cbe871286b44939e1887ca5bd51
NecroDev3 <liampatrickmcm@gmail.com>
2025-10-29 10:32:04 +0200

    tada

--- the-river-styx/app/page.tsx
+++ the-river-styx/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             My name is Alex Hitlon and I like solana 
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            this is a git commit incentive, ta da{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
